294bb431db9f53f3cd73e92f1f96120dd3be429e
Amr Essam <amr_esam@ymail.com>
2024-12-17 16:40:32 +0200

    daya04 part02

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 import day04.Day04
 
 fun main() {
-    println(Day04.part01())
+    println(Day04.part02())
 }
--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -129,4 +129,41 @@ object Day04 {
             horizontalOrReverseCount(input) +
             diagonalOrReverseCount(input))
     }
+
+    private fun xShapedMASCount(input: List<List<Char>>): Int {
+        var count = 0
+        // find the center of the X-MAS which is the letter 'A'
+        for (lineIndex in input.indices) {
+            for (charIndex in input[lineIndex].indices) {
+                // check if there's room for the X-MAS to be in the shape of an X
+                if (
+                    input[lineIndex][charIndex] == 'A' &&
+                        lineIndex - 1 >= 0 &&
+                        lineIndex + 1 < input.size &&
+                        charIndex - 1 >= 0 &&
+                        charIndex + 1 < input[lineIndex].size
+                ) {
+                    // check if there's a MAS or SAM on the diagonals forwards and backwards
+                    if (
+                        ((input[lineIndex - 1][charIndex - 1] == 'M' &&
+                            input[lineIndex + 1][charIndex + 1] == 'S') ||
+                            (input[lineIndex - 1][charIndex - 1] == 'S' &&
+                                input[lineIndex + 1][charIndex + 1] == 'M')) &&
+                            ((input[lineIndex - 1][charIndex + 1] == 'M' &&
+                                input[lineIndex + 1][charIndex - 1] == 'S') ||
+                                (input[lineIndex - 1][charIndex + 1] == 'S' &&
+                                    input[lineIndex + 1][charIndex - 1] == 'M'))
+                    ) {
+                        count++
+                    }
+                }
+            }
+        }
+        return count
+    }
+
+    fun part02(pathToInput: String = PATH_TO_INPUT): Int {
+        val input = Path(pathToInput).readLines().map { it.toList() }
+        return xShapedMASCount(input)
+    }
 }
